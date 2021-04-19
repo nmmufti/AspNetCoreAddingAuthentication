6cb5074fae236a0135f7f3b5dbaecdb4a61a698c
nmmufti <80278616+nmmufti@users.noreply.github.com>
2021-04-19 13:47:36 +0100

    Step5

--- WishList/Controllers/AccountController.cs
+++ WishList/Controllers/AccountController.cs
@@ -54,5 +54,38 @@ namespace WishList.Controllers
             }
             return RedirectToAction("Index","Home");
         }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View("Login");
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(LoginViewModel loginViewModel)
+        {
+            if (!ModelState.IsValid)
+                return View(loginViewModel);
+
+            var result = _signInManager.PasswordSignInAsync(loginViewModel.Email,loginViewModel.Password,false, false).Result;
+            if (!result.Succeeded)
+            {
+                ModelState.AddModelError(string.Empty,"Invalid login attempt")
+            }
+
+            return RedirectToAction("Index", "Item");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Logout()
+        {
+            _signInManager.SignOutAsync();
+            return RedirectToAction("Index");
+        }
+
     }
 }
